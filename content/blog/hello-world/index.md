10c7b6f9c2aecc7fe6d8ca4170407dd0137bcf78
Sebastian Thon <sthon@hotmail.de>
2019-01-11 13:16:42 +0100

    Changed font

---
+++ content/blog/hello-world/index.md
@@ -0,0 +1,9 @@
+---
+title: Willkommen zu Bastis Spielo!
+date: '2019-01-11T13:00:00.284Z'
+---
+Diese Spielothek ist voller Infos zu tollen Gesellschaftsspielen, die ich besitze.
+
+Der Grund für diese Seite ist ganz einfach: Ich habe selber inzwischen den Überblick verloren. Daher trage ich hier alle Spiele zusammen und versehen sie mit kleinen hilfreichen Beschreibungen, Links, Bewertungen und katagorisiere sie. Natürlich alles frei nach Schnauze und ohne Anspruch auf Objektivität oder Vollständigkeit.
+
+Viel Spaß beim Stöbern!
---
+++ src/pages/index.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+
+import Layout from '../components/Layout'
+import SEO from '../components/seo'
+import { rhythm } from '../utils/typography'
+
+class BlogIndex extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMarkdownRemark.edges
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title="Alle Beiträge"
+          keywords={[`blog`, `boardgames`, `brettspiele`, `gesellschaftsspiele`]}
+        />
+        {posts.map(({ node }) => {
+          const title = node.frontmatter.title || node.fields.slug
+          return (
+            <div key={node.fields.slug}>
+              <h3>
+                <Link to={node.fields.slug}>
+                  {title}
+                </Link>
+              </h3>
+              <small>{node.frontmatter.date}</small>
+              <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+            </div>
+          )
+        })}
+      </Layout>
+    )
+  }
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
